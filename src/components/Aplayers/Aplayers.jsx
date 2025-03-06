c0836f8bb6b407ee335ca29dfeceed5f56107f39
jsami31 <57608969+Sadab-7878@users.noreply.github.com>
2025-03-06 12:49:16 +0600

    tab 40%

--- src/components/Aplayers/Aplayers.jsx
+++ src/components/Aplayers/Aplayers.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Aplayers = () => {
+  const [activetab, setActivetab] = useState("Available");
   return (
     <>
       <div>
-        <div>
-            <div><p className="font-sora font-[700] text-[28px]">Available Players</p></div>
-            <div></div>
+        <div className="flex justify-between m-10">
+          <div>
+            <p className="font-sora font-[700] text-[28px] text-[#131313]">
+              Available Players
+            </p>
+          </div>
+          <div className="inline-flex border border-gray-200 rounded-2xl">
+            {["Available", "Selected (0)"].map((tab, index) => (
+              <button
+                key={tab}
+                onClick={() => setActivetab(tab)}
+                className={`py-2 px-4 transition-all cursor-pointer ${
+                  activetab === tab
+                    ?    `bg-[#E7FE29] font-bold ${ index === 0 ? "rounded-l-2xl" : "rounded-r-2xl"}`
+                    : "text-gray-500"
+                }`}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
         </div>
         <div></div>
       </div>
